58ba23d227e9f4b62fe7f255030a1f3ab1b420e7
Sagar R Pandav <sagar.rpandav@gmail.com>
2022-12-11 19:56:19 -0500

    CORS

--- distributed-final-project/web/web.go
+++ distributed-final-project/web/web.go
@@ -5,15 +5,24 @@ import (
 	auth "distributed-final-project/web/auth/middleware"
 	loginService "distributed-final-project/web/auth/server"
 	"distributed-final-project/web/globals"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
 	go loginService.CreateServer()
 	router := gin.Default()
 	router.Use(sessions.Sessions("session", cookie.NewStore(globals.Secret)))
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type"}
+	corsConfig.AllowCredentials = true
+	corsConfig.MaxAge = 12 * time.Hour
+	corsConfig.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(cors.New(corsConfig))
 
 	loginGroup := router.Group("/")
 	loginGroup.Use(auth.AuthRequired)
@@ -21,4 +30,3 @@ func main() {
 
 	router.Run("localhost:8080")
 }
-
