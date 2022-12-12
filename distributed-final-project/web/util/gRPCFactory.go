96dd9e25426b4ffe4ee4c1e43026b4bf5f584265
Irfan Ahmed <irfanahmed1802@gmail.com>
2022-12-11 23:27:52 -0500

    Adding Feed Service

--- distributed-final-project/web/util/gRPCFactory.go
+++ distributed-final-project/web/util/gRPCFactory.go
@@ -5,10 +5,11 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-var serviceMap = map[string] string{
-	"loginService": "localhost:8090",
+var serviceMap = map[string]string{
+	"loginService":    "localhost:8090",
+	"feedService":     "localhost:8091",
+	"followerService": "localhost:8092",
 }
-
 
 func GetGRPCService(serviceName string) *grpc.ClientConn {
 	dial, err := grpc.Dial(serviceMap[serviceName], grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -17,6 +18,3 @@ func GetGRPCService(serviceName string) *grpc.ClientConn {
 	}
 	return dial
 }
-
-
-
